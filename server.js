1a98659670ca5aefe94ca9f2546aad4cdef2e99d
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-06-28 01:18:08 -0700

    updated access to dotENV to create connection to mysql database

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'classlist_db'
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     },
     console.log(`Connected to the classlist_db database.`)
   );
